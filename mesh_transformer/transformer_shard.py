c028a5964c0adff2d42da599bc14759748d18f2d
mosmos6 <93764745+mosmos6@users.noreply.github.com>
2024-06-05 16:58:04 +0300

    Update transformer_shard.py

--- mesh_transformer/transformer_shard.py
+++ mesh_transformer/transformer_shard.py
@@ -52,7 +52,8 @@ class CausalTransformer:
 
         # Generate PRNG keys
         key = jax.random.PRNGKey(42)
-        keys = jax.random.split(key, mp_per_host).reshape((mp_per_host, 2))
+        keys = jax.random.split(key, mp_per_host)
+        keys = jax.vmap(lambda k: jax.random.split(k, 2))(keys)
 
         example_shape = (max(dp // jax.process_count(), 1), config["seq"],)
         x = jax.random.uniform(key, example_shape, minval=0, maxval=config["n_vocab"]).astype(jnp.uint32)  # batch, len
@@ -104,5 +105,6 @@ class CausalTransformer:
         aux = jnp.zeros((batch_size, gen_length), dtype=jnp.uint32)
         self.gen_length = gen_length
         self.return_logits = return_logits
-        keys = jax.random.split(key, batch_size).reshape((batch_size, 2))
+        keys = jax.random.split(key, batch_size)
+        keys = jax.vmap(lambda k: jax.random.split(k, 2))(keys)
         return self.generate_shmap(self.state, keys, ctx, np.array(ctx_length, dtype=np.uint32), aux, sampler_options)
