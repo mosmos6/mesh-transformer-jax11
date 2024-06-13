9209eebee4c9973689f36ceacb6e6046c1188061
mosmos6 <93764745+mosmos6@users.noreply.github.com>
2024-06-13 16:45:59 +0300

    Update layers.py

--- mesh_transformer/layers.py
+++ mesh_transformer/layers.py
@@ -152,7 +152,10 @@ def apply_rotary_pos_emb(x, sincos):
     cos = repeat(cos, 'n d -> n b h d', b=batch_size, h=num_heads)[:seq_len, :, :, :]
 
     print(f"apply_rotary_pos_emb: sin.shape = {sin.shape}, cos.shape = {cos.shape}")
-    
+
+    assert sin.shape == x.shape, f"Sin shape {sin.shape} does not match x shape {x.shape}"
+    assert cos.shape == x.shape, f"Cos shape {cos.shape} does not match x shape {x.shape}"
+
     return (x * cos) + (rotate_every_two(x) * sin)
 
 
