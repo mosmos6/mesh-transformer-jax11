0aa07fbc0a21830c256d043be4633d6d17734a69
mosmos6 <93764745+mosmos6@users.noreply.github.com>
2024-07-26 15:14:08 +0300

    Update transformer_shard.py

--- mesh_transformer/transformer_shard.py
+++ mesh_transformer/transformer_shard.py
@@ -15,6 +15,18 @@ from mesh_transformer.layers import EmbeddingShard, TransformerLayerShard, Relat
 from mesh_transformer.checkpoint import write_ckpt, read_ckpt
 
 
+class MeshContextManager:
+    def __init__(self, config):
+        self.mesh = jax.sharding.Mesh(np.array(jax.devices()).reshape(config["cores_per_replica"], -1), ("dp", "mp"))
+
+    def get_mesh(self):
+        return self.mesh
+
+    def __enter__(self):
+        self.mesh.__enter__()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.mesh.__exit__(exc_type, exc_val, exc_tb)
 
 class CausalTransformerShard(hk.Module):
     def __init__(self, config, name=None):
