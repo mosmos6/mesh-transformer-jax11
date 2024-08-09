527cc4584c2cabc5d187f55e35547948cd115b88
mosmos6 <93764745+mosmos6@users.noreply.github.com>
2024-08-09 15:58:59 +0300

    Update mesh_context_manager.py

--- mesh_transformer/mesh_context_manager.py
+++ mesh_transformer/mesh_context_manager.py
@@ -8,15 +8,17 @@ from jax.experimental import mesh_utils
 from jax.experimental.shard_map import shard_map
 from jax.sharding import Mesh, PartitionSpec as P
 
+
 class MeshContextManager:
-    def __init__(self, config):
-        self.mesh = jax.sharding.Mesh(np.array(jax.devices()).reshape(config["cores_per_replica"], -1), ("dp", "mp"))
+    def __init__(self, dp, mp):
+        devices = mesh_utils.create_device_mesh((dp, mp))
+        self.mesh = Mesh(devices, axis_names=('dp', 'mp'))
 
     def get_mesh(self):
         return self.mesh
 
     def __enter__(self):
-        self.mesh.__enter__()
+        return self.mesh.__enter__()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.mesh.__exit__(exc_type, exc_val, exc_tb)
+        return self.mesh.__exit__(exc_type, exc_val, exc_tb)
