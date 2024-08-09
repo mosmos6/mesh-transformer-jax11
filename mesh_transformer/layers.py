6b325b1832250d83f9acf6932e66ff8c95cbef67
mosmos6 <93764745+mosmos6@users.noreply.github.com>
2024-08-09 15:44:47 +0300

    Update layers.py

--- mesh_transformer/layers.py
+++ mesh_transformer/layers.py
@@ -1,27 +1,28 @@
+import flax.linen as nn
 import jax
 import jax.numpy as jnp
 import numpy as np
-import os
 from einops import rearrange, repeat
-from jax.sharding import PartitionSpec as P
-from jax.experimental.shard_map import shard_map
 
 from mesh_transformer.util import f_psum, g_psum, maybe_shard, head_print
+from jax.sharding import PartitionSpec as P
+from jax.experimental.shard_map import shard_map
 from mesh_transformer.mesh_context_manager import MeshContextManager  # Import from new file
 
-class ReplicatedLayerNorm:
-    def __init__(self, offset=True):
-        self.offset = offset
 
+class ReplicatedLayerNorm(nn.Module):
+    offset: bool = True
+
+    @nn.compact
     def __call__(self, inputs: jnp.ndarray) -> jnp.ndarray:
         mean = jnp.mean(inputs, axis=-1, keepdims=True)
         variance = jnp.var(inputs, axis=-1, keepdims=True)
 
         param_shape = inputs.shape[-1:]
-        scale = jnp.ones(param_shape, inputs.dtype)
+        scale = self.param("scale", nn.initializers.ones, param_shape)
         scale = jax.lax.all_gather(scale, "mp")[0]
 
-        offset = jnp.zeros(param_shape, inputs.dtype)
+        offset = self.param("offset", nn.initializers.zeros, param_shape)
         offset = jax.lax.all_gather(offset, "mp")[0]
 
         scale = jnp.broadcast_to(scale, inputs.shape)
@@ -34,29 +35,35 @@ class ReplicatedLayerNorm:
         else:
             return inv * (inputs - mean)
 
-class RMSNorm:
-    def __init__(self, offset, elementwise):
-        self.offset = offset
-        self.elementwise = elementwise
 
+class RMSNorm(nn.Module):
+    offset: bool
+    elementwise: bool
+
+    @nn.compact
     def __call__(self, x):
         param_shape = (x.shape[-1],) if self.elementwise else ()
         normed = x / (jnp.linalg.norm(x, axis=-1, keepdims=True) + 1e-5)
 
-        scale = jnp.full(param_shape, x.shape[-1] ** 0.5)
+        scale = self.param('scale', nn.initializers.constant(x.shape[-1] ** 0.5), param_shape)
         scale = jax.lax.pmean(scale, "mp")
         normed = normed * scale
 
         if self.offset:
-            offset = jnp.zeros(param_shape)
+            offset = self.param('offset', nn.initializers.zeros, param_shape)
             offset = jax.lax.pmean(offset, "mp")
             normed = normed + offset
 
         return normed
 
+
 def getnorm(type):
     if type == "layernorm":
         return ReplicatedLayerNorm()
+    if type == "layernorm-desync":
+        return nn.LayerNorm()
+    elif type == "layernorm-nobias":
+        return ReplicatedLayerNorm(offset=False)
     elif type == "rmsnorm":
         return RMSNorm(False, True)
     elif type == "scalenorm":
@@ -68,7 +75,11 @@ def getnorm(type):
     else:
         raise Exception("Not implemented")
 
-class RelativePositionEmbs:
+
+class RelativePositionEmbs(nn.Module):
+    num_buckets: int
+    max_distance: int
+
     @staticmethod
     def _relative_position_bucket(relative_position, num_buckets=32, max_distance=128):
         ret = 0
@@ -77,25 +88,28 @@ class RelativePositionEmbs:
         max_exact = num_buckets // 2
         is_small = (n < max_exact)
         val_if_large = max_exact + (
-            np.log(n.astype(np.float32) / max_exact + np.finfo(np.float32).eps) /
-            np.log(max_distance / max_exact) *
-            (num_buckets - max_exact)).astype(np.int32)
+                np.log(n.astype(np.float32) / max_exact + np.finfo(np.float32).eps) /
+                np.log(max_distance / max_exact) *
+                (num_buckets - max_exact)).astype(np.int32)
         val_if_large = np.minimum(val_if_large, num_buckets - 1)
         ret += np.where(is_small, n, val_if_large)
         return ret
 
-    def __call__(self, qlen, klen, heads, num_buckets):
+    @nn.compact
+    def __call__(self, qlen, klen, heads):
         context_position = np.arange(qlen, dtype=jnp.int32)[:, None]
         memory_position = np.arange(klen, dtype=jnp.int32)[None, :]
         relative_position = memory_position - context_position
-        rp_bucket = self._relative_position_bucket(relative_position)
-        relative_attention_bias = jnp.ones((heads, num_buckets)) * 0.02  # Initialize to some value
-
-        bcast_iota = jax.lax.broadcasted_iota(jnp.int32, (num_buckets, 1, 1), 0)
+        rp_bucket = self._relative_position_bucket(relative_position, self.num_buckets, self.max_distance)
+        relative_attention_bias = self.param('rel_embedding', nn.initializers.truncated_normal(stddev=0.02), [heads, self.num_buckets])
+        bcast_iota = jax.lax.broadcasted_iota(jnp.int32, (self.num_buckets, 1, 1), 0)
         rp_bucket_one_hot = jnp.array(rp_bucket[jnp.newaxis, Ellipsis] == bcast_iota).astype(relative_attention_bias.dtype)
-        values = jax.lax.dot_general(relative_attention_bias, rp_bucket_one_hot,
-                                     (((1,), (0,)), ((), ())))
+        values = jax.lax.dot_general(
+            relative_attention_bias,
+            rp_bucket_one_hot,
+            (((1,), (0,)), ((), ())))
         return values
+
 
 def fixed_pos_embedding(seq_len, dim):
     inv_freq = 1. / (10000 ** (np.arange(0, dim, 2) / dim))
@@ -107,11 +121,13 @@ def fixed_pos_embedding(seq_len, dim):
     cos = np.concatenate([cos, cos], axis=-1)
     return jnp.array(sin, dtype=jnp.float32), jnp.array(cos, dtype=jnp.float32)
 
+
 def rotate_every_two(x):
     x1 = x[:, :, :, ::2]
     x2 = x[:, :, :, 1::2]
     x = jnp.stack((-x2, x1), axis=-1)
     return rearrange(x, '... d j -> ... (d j)')
+
 
 def apply_rotary_pos_emb(x, sincos):
     sin, cos = sincos
@@ -120,11 +136,14 @@ def apply_rotary_pos_emb(x, sincos):
     cos = repeat(cos, 'n d -> n b h d', b=batch_size, h=num_heads)[:, :, :, :head_dim]
     return (x * cos) + (rotate_every_two(x) * sin)
 
-class EmbeddingShard:
-    def __init__(self, n_vocab, d_model, cores_per_replica, seq_len, pe):
-        in_dim = n_vocab
-        out_dim = d_model
-        shards = cores_per_replica
+
+class EmbeddingShard(nn.Module):
+    config: dict
+
+    def setup(self):
+        in_dim = self.config["n_vocab"]
+        out_dim = self.config["d_model"]
+        shards = self.config["cores_per_replica"]
 
         assert in_dim % shards == 0
 
@@ -133,12 +152,13 @@ class EmbeddingShard:
         self.in_dim_per_shard = in_dim // shards
         self.out_dim_per_shard = out_dim // shards
 
-        if pe == "fixed":
-            self.positional_embeddings = np.random.normal(0, 0.02, (seq_len, self.out_dim_per_shard))
+        if self.config["pe"] == "fixed":
+            embed_init = nn.initializers.truncated_normal(stddev=0.02)
+            self.positional_embeddings = self.param('pos_embs', embed_init, [self.config["seq"], self.out_dim_per_shard])
         else:
             self.positional_embeddings = None
 
-        self.proj = jax.nn.Dense(self.out_dim)
+        self.proj = nn.Dense(self.out_dim, kernel_init=nn.initializers.truncated_normal(stddev=1 / np.sqrt(in_dim)))
 
     def __call__(self, x, dtype=jnp.bfloat16):
         shard_start_index = jax.lax.axis_index('mp') * self.in_dim_per_shard
@@ -150,31 +170,23 @@ class EmbeddingShard:
 
         if self.positional_embeddings is not None:
             all_pos_embed = jax.lax.all_gather(self.positional_embeddings, 'mp')
-            all_pos_embed = jnp.transpose(all_pos_embed, (1, 0, 2)).reshape(-1, self.out_dim_per_shard)
+            all_pos_embed = all_pos_embed.reshape(self.config["seq"], -1)
             proj_out += all_pos_embed
 
         return proj_out
 
-import jax
-import jax.numpy as jnp
-import numpy as np
-from einops import rearrange, repeat
 
-from mesh_transformer.util import f_psum, g_psum, maybe_shard, head_print
-from jax.sharding import PartitionSpec as P
-from jax.experimental.shard_map import shard_map
-from mesh_transformer.mesh_context_manager import MeshContextManager  # Import from new file
+class TransformerLayerShard(nn.Module):
+    config: dict
+    mesh_manager: MeshContextManager
+    init_scale: float = 1.0
 
-
-class TransformerLayerShard:
-    def __init__(self, config, mesh_manager, init_scale=1.0):
-        self.config = config
-        self.mesh_manager = mesh_manager
-        heads = config["n_heads"]
-        dim = config["d_model"]
-        shards = config["cores_per_replica"]
-        norm = getnorm(config["norm"])
-        self.is_rotary = config["pe"] == "rotary"
+    def setup(self):
+        heads = self.config["n_heads"]
+        dim = self.config["d_model"]
+        shards = self.config["cores_per_replica"]
+        norm = getnorm(self.config["norm"])
+        self.is_rotary = self.config["pe"] == "rotary"
 
         assert dim % heads == 0
         assert heads % shards == 0
@@ -183,23 +195,21 @@ class TransformerLayerShard:
         self.dim_per_head = dim // heads
         self.heads_per_shard = heads // shards
         self.dim_per_shard = dim // shards
-        self.pe_rotary_dims = config.get("pe_rotary_dims", self.dim_per_head)
+        self.pe_rotary_dims = self.config.get("pe_rotary_dims", self.dim_per_head)
 
         self.norm = norm
 
-        self.q = stax.Dense(self.dim_per_shard, W_init=jax.nn.initializers.normal(), b_init=None)
-        self.v = stax.Dense(self.dim_per_shard, W_init=jax.nn.initializers.normal(), b_init=None)
-        self.k = stax.Dense(self.dim_per_shard, W_init=jax.nn.initializers.normal(), b_init=None)
+        self.q = nn.Dense(self.dim_per_shard, use_bias=False)
+        self.v = nn.Dense(self.dim_per_shard, use_bias=False)
+        self.k = nn.Dense(self.dim_per_shard, use_bias=False)
 
-        self.o = stax.Dense(self.dim, W_init=jax.nn.initializers.truncated_normal(stddev=init_scale / np.sqrt(self.dim)), b_init=None)
+        self.o = nn.Dense(self.dim, use_bias=False, kernel_init=nn.initializers.truncated_normal(stddev=self.init_scale / np.sqrt(self.dim)))
 
-        self.dense_proj = stax.Dense(self.dim_per_shard * 4)
-        self.dense_proj_o = stax.Dense(self.dim,
-                                       W_init=jax.nn.initializers.truncated_normal(stddev=init_scale / np.sqrt(self.dim)), b_init=None)
+        self.dense_proj = nn.Dense(self.dim_per_shard * 4)
+        self.dense_proj_o = nn.Dense(self.dim, kernel_init=nn.initializers.truncated_normal(stddev=self.init_scale / np.sqrt(self.dim)))
 
     def self_attn(self, q, v, k, attn_bias):
         if self.is_rotary:
-            print(f"self_attn: q.shape = {q.shape}, k.shape = {k.shape}")
             k_rot = k[:, :, :, :self.pe_rotary_dims]
             k_pass = k[:, :, :, self.pe_rotary_dims:]
 
@@ -216,11 +226,9 @@ class TransformerLayerShard:
             q = jnp.concatenate([q_rot, q_pass], axis=-1)
 
         attention_logits = jnp.einsum("bthd,bThd->bhtT", q, k)
-        print(f"attention_logits shape: {attention_logits.shape}")
 
         sqrt_key_size = np.sqrt(self.dim_per_head).astype(k.dtype)
         attention_logits = attention_logits / sqrt_key_size
-        print(f"attention_logits normalized shape: {attention_logits.shape}")
 
         attention_logits += attn_bias
 
@@ -242,7 +250,6 @@ class TransformerLayerShard:
         return q, v, k
 
     def __call__(self, x, attn_bias):
-        print(f"Available axis names: {self.mesh.axis_names}")  # Debug print
         x = jax.lax.psum(x, axis_name='mp')
         x = self.norm(x)
         q, v, k = self.qvk_proj(x)
@@ -284,14 +291,8 @@ class TransformerLayerShard:
         }
 
     def get_init_decode_state(self, x, given_length, attn_bias):
-        
-        mesh = self.mesh_manager.get_mesh()  # Use the already initialized MeshContextManager
-        print(f"Mesh devices: {mesh.devices}")
-        print(f"Mesh axis names: {mesh.axis_names}")
-
-        with mesh:  # Ensure the mesh context is active
-                      
-            print("Entering mesh context")
+        mesh = self.mesh_manager.get_mesh()
+        with mesh:
             x = jax.lax.psum(x, 'mp')
             x = self.norm(x)
 
@@ -313,14 +314,13 @@ class TransformerLayerShard:
         return attn_out + dense_out, {"k": k, "v": v, "tokens_decoded": given_length.astype(jnp.uint32)}
 
 
-def compute_shard_start_index(dim_per_shard):
-    return jax.lax.axis_index('mp') * dim_per_shard
+class ProjectionShard(nn.Module):
+    config: dict
 
-class ProjectionShard:
-    def __init__(self, config):
-        self.dim_per_shard = config["dim_per_shard"]
-        self.out_dim = config["out_dim"]
-        self.shards = config["cores_per_replica"]
+    def setup(self):
+        self.dim_per_shard = self.config["dim_per_shard"]
+        self.out_dim = self.config["out_dim"]
+        self.shards = self.config["cores_per_replica"]
         self.mesh = jax.sharding.Mesh(np.array(jax.devices()).reshape(self.shards, -1), ("dp", "mp"))
 
     def loss(self, x, target, shard_start_index, z_loss):
@@ -342,15 +342,19 @@ class ProjectionShard:
         return -(softmax_logits.mean() + z_loss_penalty), jnp.argmax(logits, axis=-1) == target
 
     def forward(self, x):
-        x = jax.nn.LayerNorm(axis=-1, create_scale=True, create_offset=True)(x)
-        x = jax.nn.Dense(self.out_dim)(x)
+        x = nn.LayerNorm(axis=-1, create_scale=True, create_offset=True)(x)
+        x = nn.Dense(self.out_dim)(x)
         return x
 
-class Projection:
-    def __init__(self, config):
-        self.dim = config["n_vocab"]
-        self.norm = jax.nn.LayerNorm(axis=-1, create_scale=True, create_offset=True)
-        self.proj = jax.nn.Dense(self.dim)
+
+class Projection(nn.Module):
+    config: dict
+
+    def setup(self):
+        self.dim = self.config["n_vocab"]
+        self.norm = nn.LayerNorm()
+
+        self.proj = nn.Dense(self.dim)
 
     def __call__(self, x):
         x = self.norm(x)
@@ -373,3 +377,7 @@ class Projection:
         loss += (1e-4 * jnp.square(jnp.log(sum_exp_logits)) * z_loss).mean()
         correct = (0.0 == predicted_logits)
         return loss, correct
+
+
+def compute_shard_start_index(dim_per_shard):
+    return jax.lax.axis_index('mp') * dim_per_shard
