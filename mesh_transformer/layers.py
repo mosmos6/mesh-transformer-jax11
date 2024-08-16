8fb1a023045875bf0cffc87bd8498a86b380680e
mosmos6 <93764745+mosmos6@users.noreply.github.com>
2024-08-16 15:31:16 +0300

    Update layers.py

--- mesh_transformer/layers.py
+++ mesh_transformer/layers.py
@@ -111,28 +111,26 @@ class RelativePositionEmbs(nn.Module):
         return values
 
 
-def fixed_pos_embedding(seq_len, dim):
+def fixed_pos_embedding(x, seq_dim=0):
+    dim = x.shape[-1]
     inv_freq = 1. / (10000 ** (np.arange(0, dim, 2) / dim))
-    position = np.arange(0, seq_len, dtype=np.float32)
-    sinusoid_inp = np.einsum('i,j->ij', position, inv_freq)
-    
-    sin = np.sin(sinusoid_inp)
-    cos = np.cos(sinusoid_inp)
-    
-    return sin, cos
+
+    sinusoid_inp = np.einsum('i , j -> i j', np.arange(x.shape[seq_dim]), inv_freq)
+
+    return np.sin(sinusoid_inp), np.cos(sinusoid_inp)
+
 
 def rotate_every_two(x):
-    x1 = x[..., ::2]
-    x2 = x[..., 1::2]
+    x1 = x[:, :, ::2]
+    x2 = x[:, :, 1::2]
 
     x = jnp.stack((-x2, x1), axis=-1)
+
     return rearrange(x, '... d j -> ... (d j)')
 
+
 def apply_rotary_pos_emb(x, sincos):
-    sin, cos = sincos
-    sin = repeat(sin, 'n d -> n 1 d', d=x.shape[-1])
-    cos = repeat(cos, 'n d -> n 1 d', d=x.shape[-1])
-    
+    sin, cos = map(lambda t: repeat(t, 'b n -> b (n j)', j=2)[-x.shape[0]:, None, :], sincos)
     return (x * cos) + (rotate_every_two(x) * sin)
 
 
