12e467783e9e313dc4ebd6a8d2f331477e54f74d
mosmos6 <93764745+mosmos6@users.noreply.github.com>
2024-11-20 16:04:41 +0200

    Update transformer_shard.py

--- mesh_transformer/transformer_shard.py
+++ mesh_transformer/transformer_shard.py
@@ -180,7 +180,7 @@ class CausalTransformer:
         self.init_shmap = jax.jit(shard_map(
             vmapped_fn,  # Use the vmapped version of the function
             in_specs=(P('dp')),  # Don't shard rng, shard input over mp
-            out_specs=(P(), P('mp', 'dp')),  
+            out_specs=(P('dp')),  
             mesh=mesh_manager.get_mesh()
         ))
 
